48c59f16c0284869ff3ddcb50c77c00a2f311f77
Pankaj Goyal <ar-pankaj.goyal@rakuten.com>
2022-02-25 01:53:03 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Item from "./Components/Card";
+import rawData from "./assets/data.json";
+import { useEffect, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import { Button, Box } from '@mui/material';
+const allTags = rawData.reduce((arr,item)=> { item.tags.forEach((t)=>arr.add(t)); return arr;},new Set());
 function App() {
+  const [data,setData] = useState(rawData);
+  const [listTags,setListTags] = useState(Array.from(allTags));
+  const [tags, setTags] = useState([]);
+  useEffect(()=>{
+    setListTags((prev)=>{
+      const ns = Array.from(allTags).filter((lt)=> !(tags.includes(lt)) );
+      return ns;
+    });
+  },[tags]);
+  const handleChange = (event) => {
+    setTags([event.target.value,...tags]);
+  };
+  let filteredList = data.filter((item)=> item.tags.some((t)=>tags.includes(t)));
+  if(tags.length === 0) filteredList=data;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        Phaidra
+      </div>
+      <div className='searchbar'>
+        <TextField
+            id="standard-select-currency"
+            select
+            label="Select"
+            value={""}
+            onChange={handleChange}
+            helperText="Please Select tags"
+            variant="standard"
+          >
+            {listTags.map((option) => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </TextField>
+      </div>
+      <div style={{display:"flex", flexDirection:"row"}}>
+        {tags.map((t)=>{
+          return (
+            <Box variant='contained' margin='10px' >
+              {t}
+              <Button variant='contained' onClick={(e)=>{
+                const ns = tags.filter((tg)=> tg !== t);
+                setTags(ns);
+              }}>X</Button>  
+            </Box>
+          );
+        })}
+      </div>
+      <div className='task'>
+        {
+            filteredList.map((item)=>{
+              return(
+                <Item key={item.id} item={item} tags={tags} setTags={setTags} />
+              );
+            })
+        }
+      </div>
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { Button, Box } from '@mui/material';
+import Typography from '@mui/material/Typography';
+export default function Item(props) {
+  const {item}  = props;
+  return (
+    <Card key={item.id} sx={{ display:'flex' ,flexDirection:'row',justifyContent:'space-between', border:'5px',margin:'10px' }}>
+      <CardMedia
+        component="img"
+        sx={{heigth:'35px',maxWidth:'100px'}}
+        src={require(""+item.logo)}
+        alt="ImageAlt"
+      />
+    <Box>
+        <Box display={"flex"} flexDirection={"row"} >
+            <Typography gutterBottom variant="body2" component="div">
+                {item.company}
+            </Typography>
+            {
+                item.new && (
+                    <Button variant={"contained"}>New!</Button>
+                )
+            }
+            {
+                item.featured && (
+                    <Button variant={"contained"}>Featured</Button>
+                )
+            }
+        </Box>
+        <Typography variant="h5" color="text"> 
+            {item.position}
+        </Typography>
+        <Box display={"flex"} flexDirection={"row"} >
+            <Typography variant="body2" color="text.secondary"> 
+            {item.postedAt+ "  - "}
+            </Typography>
+            <Typography variant="body2" color="text.secondary"> 
+            {item.type+ "  - "}
+            </Typography>
+            <Typography variant="body2" color="text.secondary"> 
+            {item.location}
+            </Typography>
+        </Box>
+    </Box>
+      <CardContent sx={{display:'flex',justifyContent:'space-between'}}>
+        <Typography variant="h5" color="text.secondary"> 
+            {item.role}
+        </Typography>
+        <Typography variant="h5" color="text"> 
+            {item.level}
+        </Typography>
+        {
+            item.tags.map((tag)=> <Button key={tag} variant={"contained"}>{tag}</Button>)
+        }
+      </CardContent>
+    </Card>
+  );
+}
